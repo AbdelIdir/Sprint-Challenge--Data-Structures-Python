662afba2717f573a58366c351522224e40ace015
AbdelI <homeworkoracle@protonmail.com>
2020-04-24 11:48:13 +0200

    ring buffer,test passing, MVP done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,27 +6,43 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        self.size = 0
 
     def append(self, item):
-        pass
+
+        if self.storage.length == self.capacity:
+            self.current.value = item
+            self.current = self.current.next
+        else:
+            self.storage.add_to_tail(item)
+            if self.storage.length == 1:
+                self.current = self.storage.head
+            if self.storage.length == self.capacity:
+                self.storage.tail.next = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        view = self.storage.head
+        list_buffer_contents.append(view.value)
 
+        while view.next != self.storage.head:
+            if not view.next:
+                break
+            list_buffer_contents.append(view.next.value)
+            view = view.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
 
 
-class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+# class ArrayRingBuffer:
+#     def __init__(self, capacity):
+#         pass
 
-    def append(self, item):
-        pass
+#     def append(self, item):
+#         pass
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
